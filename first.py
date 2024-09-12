a72f02e68a096138e412ffc1f4045447634b1c44
kasid786 <kasid9990@gamil.com>
2024-09-12 12:21:45 +0530

    myfile

---
+++ first.py
@@ -0,0 +1,14 @@
+print("hello wctm")
+q1=50000
+q2=75000
+q3=60000
+q4=65000
+sum=q1+q2+q3+q4
+print(sum)
+print(type(q1))
+a="kasid hashmi"
+print(type(a))
+b=True
+c=False
+print(type(b))
+print(type(c))
